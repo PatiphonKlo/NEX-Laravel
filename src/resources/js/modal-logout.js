546d131f4330fd231ad9f995322eb341d497b1b1
Patiphon <patiphon.miscible@gmail.com>
2024-07-11 13:51:28 +0700

    home page fix1

--- src/resources/js/modal-logout.js
+++ src/resources/js/modal-logout.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     var modal = document.getElementById("logoutModal");
     var button = document.getElementById("logoutButton");
-    var closeModalButton = document.getElementById("closeModalButton"); 
 
     button.addEventListener("click", function (event) {
         modal.classList.remove("hidden");
-        event.stopPropagation(); // ป้องกันการเกิด event bubbling
+        event.stopPropagation(); 
     });
 
     window.addEventListener("click", function (event) {
