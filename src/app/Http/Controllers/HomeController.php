f589dffc9389567af7fe3da6503d8aa7a14141b0
Patiphon <patiphon.miscible@gmail.com>
2024-07-11 13:36:31 +0700

    home page fix

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -6,13 +6,32 @@ use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
+
     public function index()
     {
         try {
+            if (!Session::has('auth_failed')) {
+                Session::put('auth_failed', false);
+            }
+
+            if (!Session::has('uid')) {
+                Session::put('uid', null);
+            }
+
+            if (!Session::has('role')) {
+                Session::put('role', 'guest');
+            }
+
+            if (!Session::has('isAuthenticated')) {
+                Session::put('isAuthenticated', false);
+            }
+
             $showModal = Session::get('auth_failed', false);
             $authenticated = Session::has('uid');
             $role = Session::get('role', 'guest');
-            return view('pages/user/home', compact('showModal', 'authenticated', 'role'));
+            $isAuthenticated = Session::get('isAuthenticated', false);
+
+            return view('pages/user/home', compact('showModal', 'authenticated', 'role', 'isAuthenticated'));
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
